43241bd714fa3bd2ab4a0bb743360fd295669c95
nabin <nkbhaie2017@gmail.com>
2024-06-11 14:04:42 -0600

    initial commit

---
+++ src/main/java/com/webprojects/springboot/spring_boot_projects/SpringBootProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.webprojects.springboot.spring_boot_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webprojects/springboot/spring_boot_projects/SpringBootProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webprojects.springboot.spring_boot_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
